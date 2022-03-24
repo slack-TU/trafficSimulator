e0aaf0c818a36e5b53bce2d9d0a0e3f94227e025
slack-TU <67699394+slack-TU@users.noreply.github.com>
2022-03-24 18:37:39 -0400

    yoshi's castle
    worst mario kart course ever

--- src/sc_test.py
+++ src/sc_test.py
@@ -5,41 +5,46 @@ sim = Simulation()
 
 # Add multiple roads
 sim.create_roads([
-    ((75, 100), (75, 150)), #vertical middle, 
+
+    #cross roads
+    ((75, 100), (75, 150)), #vertical middle, 0
     ((75, 150), (75, 210)),
-    ((0, 150), (75, 150)), #horizontal middle, 
+    ((0, 150), (75, 150)), #horizontal middle, 2
     ((75, 150), (150, 150)),  
 
-    ((10, 100), (75, 100)), #top,
+    #circuit
+    ((10, 100), (75, 100)), #top, 4
     ((75, 100), (140, 100)),  
-    ((150, 110), (150, 150)), #right, 
+
+    ((150, 110), (150, 150)), #right 6
     ((150, 150), (150, 200)),
-    ((140, 210), (75, 210)), #bottom, 
+
+    ((140, 210), (75, 210)), #bottom 8
     ((75, 210), (10, 210)), 
-    ((0, 200), (0, 150)),   #left, 
+
+    ((0, 200), (0, 150)),   #left, 10
     ((0, 150), (0, 110)),
 
-    *curve_road((140, 100), (150, 110), (150, 100)), #top to right, 
-    *curve_road((150,200), (140, 210), (150, 210)), # right to bottom 
-    *curve_road((10,210), (0, 200), (0, 210)),  #bottom to left 
-    *curve_road((0,110), (10, 100), (0, 100))   #left to top 
-
-
+    *curve_road((140, 100), (150, 110), (150, 100)), #top to right, 12-26
+    *curve_road((150,200), (140, 210), (150, 210)), # right to bottom 27-41 
+    *curve_road((10,210), (0, 200), (0, 210)),  #bottom to left , 42- 56
+    *curve_road((0,110), (10, 100), (0, 100))   #left to top , 57-71
 ])
 
 sim.create_gen({
-    'vehicle_rate': 20,
+    'vehicle_rate': 50,
     'vehicles': [
-        [1, {"path": [0, 1]}],
-        [1, {"path": [0, 3]}],
-        [1, {"path": [2, 3]}],
-        [1, {"path": [2, 3]}]
+        [1, {"path": [4, 5, *range(12, 26), 6, 7, *range(27, 41), 8, 9, *range(42, 56), 10, 11, *range(57, 71), 4, 0, 1]}],
+        [1, {"path": [4, 5, *range(12, 26), 6, 7, *range(27, 41), 8, 9, *range(42, 56), 10, 2, 3]}]
     ]
 })
 
 sim.create_signal([[0], [2]])
-sim.create_signal([[3], [6]])
 sim.create_signal([[1], [8]])
+sim.create_signal([[6], [3]])
+sim.create_signal([[4], [10]])
+
+
 
 
 # Start simulation
